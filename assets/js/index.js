70eb8d1d1f144c32aa1c5d310de61b39d4e1dbb2
Aurélien <aurelien.monceau@gmail.com>
2022-10-08 17:39:23 +0200

    Modification

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,9 +9,9 @@ const scroll = new LocomotiveScroll({
 scroll.on("scroll", () => {
   if (document.querySelector("#color.is-inview")) {
     document.body.style.background = radial-gradient(red, rgb(87, 87, 221));
-    document.body.style.color = fefeff;
+    document.body.style.color = "#fefeff";
   } else {
-    document.body.style.background = fefeff;
-    document.body.style.color = fefeff;
+    document.body.style.background = "black";
+    document.body.style.color = "#000101";
   }
 });
