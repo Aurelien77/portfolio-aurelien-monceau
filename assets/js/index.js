76db355e079d62d660568f822a2d59b3613ef90d
Aurélien <aurelien.monceau@gmail.com>
2023-01-09 22:08:17 +0100

    Push back

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,9 +9,9 @@ const scroll = new LocomotiveScroll({
 scroll.on("scroll", () => {
   if (document.querySelector("#color.is-inview")) {
     document.body.style.background = radial-gradient(red, rgb(87, 87, 221));
-    document.body.style.color = "#fefeff";
+    document.body.style.color = radial-gradient(rgb(14, 13, 13), rgb(72, 72, 77));
   } else {
     document.body.style.background = "black";
-    document.body.style.color = "#000101";
+    document.body.style.color = "black";
   }
 });
